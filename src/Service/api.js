2fb6a065b868d61c52b39d79ca69a0d00fcc30e3
atul patil <atulpatil539@gmail.com>
2021-10-30 23:20:09 +0530

    Fix : heruku deploy

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = "http://localhost:3002/users"
+const url = "https://userinfo-fix.herokuapp.com/users"
 
 export const getUsers = async (id) => {
     id = id || '';
