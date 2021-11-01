fb576bddbf42e5631620e942a438c22ae7ae87cd
atul patil <atulpatil539@gmail.com>
2021-11-01 12:58:23 +0530

    feat:updated

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = "https://userinfo-fix.herokuapp.com/api/users"
+const url = "http://localhost:3002/users"
 
 export const getUsers = async (id) => {
     id = id || '';
